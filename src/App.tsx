5cfdec37e9cd506b89cbd72542e289940c38ef29
EijiGorilla <matsuzakieiji0@gmail.com>
2023-11-09 15:48:57 +0800

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,386 @@
+/* eslint-disable react/jsx-no-comment-textnodes */
+import { useRef, useEffect, useState } from 'react';
+import Select from 'react-select';
+import { map, view, basemaps, layerList } from './Scene';
+import './index.css';
+import './App.css';
+import '@esri/calcite-components/dist/components/calcite-shell';
+import '@esri/calcite-components/dist/components/calcite-list';
+import '@esri/calcite-components/dist/components/calcite-list-item';
+import '@esri/calcite-components/dist/components/calcite-shell-panel';
+import '@esri/calcite-components/dist/components/calcite-action';
+import '@esri/calcite-components/dist/components/calcite-action-bar';
+import '@esri/calcite-components/dist/calcite/calcite.css';
+import {
+  CalciteShell,
+  CalciteShellPanel,
+  CalciteActionBar,
+  CalciteAction,
+  CalciteSwitch,
+  CalciteTabs,
+  CalcitePanel,
+  CalciteList,
+  CalciteListItem,
+} from '@esri/calcite-components-react';
+import { dropdownQuery } from './Query';
+import Chart from './components/Chart';
+
+function App() {
+  const mapDiv = useRef(null);
+  const layerListDiv = useRef<HTMLDivElement | undefined | any>(null);
+
+  // For Calcite Design
+  const calcitePanelBasemaps = useRef<HTMLDivElement | undefined | any>(null);
+  const [activeWidget, setActiveWidget] = useState<undefined | any | unknown>(null);
+  const [nextWidget, setNextWidget] = useState<undefined | any | unknown>(null);
+
+  // Calcite switch for see-through-ground
+  const [underground, setUnderground] = useState<boolean>(false);
+
+  // For dropdown filter
+  const [initContractPacakgeCompType, setInitContractPacakgeCompType] = useState([
+    {
+      cp: '',
+      company: [
+        {
+          name: '',
+          type: [
+            {
+              typeName: '',
+            },
+          ],
+        },
+      ],
+    },
+  ]);
+
+  const [contractPackage, setContractPackage] = useState(null);
+  const [company, setCompany] = useState(null);
+  const [type, setType] = useState<null | undefined | string>(null);
+
+  const [companyList, setCompanyList] = useState([]);
+  const [typeList, setTypeList] = useState();
+  const [companySelected, setCompanySelected] = useState({ name: '' });
+
+  // Create dropdown list//
+  useEffect(() => {
+    dropdownQuery().then((response: any) => {
+      // 1. unique value for cp
+      const uniqueCP = response
+        .map((item: any) => item.cp)
+        .filter((cp: any, index: any, emp: any) => emp.indexOf(cp) === index);
+
+      // 2. Compile for each CP
+      // eslint-disable-next-line array-callback-return
+      const finalArray = uniqueCP.map((contp: any, index: number) => {
+        // 2.1. unique vales for company corresponding to each CP
+        const filteredCp = response.filter((emp: any) => emp.cp === contp);
+        const uniqueCompany = filteredCp
+          .map((item: any) => item.company)
+          .filter((company: any, index: any, emp: any) => emp.indexOf(company) === index);
+
+        // 2.2. unique values for corresponding Type to each CP and each company
+        const companyType = uniqueCompany.map((comp: any, index: any) => {
+          // 2.2.1. Find type(s) corresponding to each CP and company
+          const filtered = response.filter((emp: any) => emp.cp === contp && emp.company === comp);
+          const types = filtered.map((item: any, index: number) => item.type);
+
+          // 2.2.2. Check a pair of CP and Company has one type or both types (point and line)
+          return Object.assign({
+            name: comp,
+            type:
+              types.length === 1
+                ? [{ typeName: types }]
+                : [{ typeName: types[0] }, { typeName: types[1] }],
+          });
+        });
+
+        return Object.assign({
+          cp: contp,
+          company: companyType,
+        });
+      });
+
+      setInitContractPacakgeCompType(finalArray);
+    });
+  }, []);
+
+  const handleContractPackageChange = (obj: any) => {
+    setContractPackage(obj);
+    setCompanyList(obj.company);
+    setCompany(null);
+    setCompanySelected(obj);
+    setType(null);
+  };
+
+  const handleCompanyChange = (obj: any) => {
+    setCompanySelected(obj);
+    setCompany(obj);
+    setTypeList(obj.type);
+    setType(null);
+  };
+
+  const handleTypeChange = (obj: any) => {
+    setType(obj);
+  };
+
+  useEffect(() => {
+    if (activeWidget) {
+      const actionActiveWidget = document.querySelector(
+        `[data-panel-id=${activeWidget}]`,
+      ) as HTMLCalcitePanelElement;
+      actionActiveWidget.hidden = true;
+    }
+
+    if (nextWidget !== activeWidget) {
+      const actionNextWidget = document.querySelector(
+        `[data-panel-id=${nextWidget}]`,
+      ) as HTMLCalcitePanelElement;
+      actionNextWidget.hidden = false;
+    }
+  });
+
+  useEffect(() => {
+    map.ground.opacity = underground === true ? 0.7 : 1;
+    view.environment.atmosphereEnabled = false;
+  }, [underground]);
+
+  useEffect(() => {
+    if (mapDiv.current) {
+      map.ground.navigationConstraint = {
+        type: 'none',
+      };
+
+      view.container = mapDiv.current;
+      view.ui.components = [];
+      view.ui.empty('top-left');
+      basemaps.container = calcitePanelBasemaps.current;
+      layerList.container = layerListDiv.current;
+    }
+  }, []);
+
+  // Style CSS
+  const customstyles = {
+    option: (styles: any, { data, isDisabled, isFocused, isSelected }: any) => {
+      // const color = chroma(data.color);
+      return {
+        ...styles,
+        backgroundColor: isFocused ? '#555555' : isSelected ? '#2b2b2b' : '#2b2b2b',
+        color: '#ffffff',
+      };
+    },
+
+    control: (defaultStyles: any) => ({
+      ...defaultStyles,
+      backgroundColor: '#2b2b2b',
+      borderColor: '#949494',
+      height: 35,
+      width: '170px',
+      color: '#ffffff',
+    }),
+    singleValue: (defaultStyles: any) => ({ ...defaultStyles, color: '#fff' }),
+  };
+  //          <Chart contractp={contractPackageSelected.cp} company={companySelected.name} type={type === null ? '' : type}/>
+  return (
+    <>
+      <CalciteShell>
+        <CalciteTabs slot="panel-end" style={{ width: '25vw' }}>
+          <Chart
+            contractp={contractPackage === null ? '' : contractPackage}
+            company={companySelected.name}
+            type={type === null ? '' : type}
+            typelist={typeList}
+          />
+        </CalciteTabs>
+        <header
+          slot="header"
+          id="header-title"
+          style={{ display: 'flex', width: '100%', padding: '0 1rem' }}
+        >
+          <img
+            src="https://EijiGorilla.github.io/Symbols/Projec_Logo/DOTr_Logo_v2.png"
+            alt="DOTr Logo"
+            height={'2.9%'}
+            width={'2.9%'}
+            style={{ marginBottom: 'auto', marginTop: 'auto' }}
+          />
+          <b className="headerTitle">N2 UTILITY</b>
+          <div className="date">As of October 25, 2023</div>
+
+          <div className="dropdownFilter">
+            <div className="dropdownFilterLayout">
+              <b style={{ color: 'white', margin: 10, fontSize: '0.9vw' }}></b>
+              <Select
+                placeholder="Select CP"
+                value={contractPackage}
+                options={initContractPacakgeCompType}
+                onChange={handleContractPackageChange}
+                getOptionLabel={(x: any) => x.cp}
+                styles={customstyles}
+              />
+              <br />
+              <b style={{ color: 'white', margin: 10, fontSize: '0.9vw' }}></b>
+              <Select
+                placeholder="Select Company"
+                value={company}
+                options={companyList}
+                onChange={handleCompanyChange}
+                getOptionLabel={(x: any) => x.name}
+                styles={customstyles}
+              />
+              <br />
+              <b style={{ color: 'white', margin: 10, fontSize: '0.9vw' }}></b>
+              <Select
+                placeholder="Select Type"
+                value={type}
+                options={typeList}
+                onChange={handleTypeChange}
+                getOptionLabel={(x: any) => x.typeName}
+                styles={customstyles}
+              />
+            </div>
+          </div>
+          <img
+            src="https://EijiGorilla.github.io/Symbols/Projec_Logo/GCR LOGO.png"
+            alt="GCR Logo"
+            height={'4.4%'}
+            width={'4.4%'}
+            style={{
+              marginBottom: 'auto',
+              marginTop: 'auto',
+              marginLeft: 'auto',
+              marginRight: '5rem',
+            }}
+          />
+        </header>
+        {/* Calcite Action Panel */}
+        <CalciteShellPanel
+          width-scale="1"
+          slot="panel-start"
+          position="start"
+          id="left-shell-panel"
+          displayMode="dock"
+        >
+          <CalciteActionBar slot="action-bar">
+            <CalciteAction
+              data-action-id="layers"
+              icon="layers"
+              text="layers"
+              id="layers"
+              //textEnabled={true}
+              onClick={(event: any) => {
+                setNextWidget(event.target.id);
+                setActiveWidget(nextWidget === activeWidget ? null : nextWidget);
+              }}
+            ></CalciteAction>
+
+            <CalciteAction
+              data-action-id="basemaps"
+              icon="basemap"
+              text="basemaps"
+              id="basemaps"
+              onClick={(event: any) => {
+                setNextWidget(event.target.id);
+                setActiveWidget(nextWidget === activeWidget ? null : nextWidget);
+              }}
+            ></CalciteAction>
+
+            <CalciteAction
+              data-action-id="information"
+              icon="information"
+              text="Information"
+              id="information"
+              onClick={(event: any) => {
+                setNextWidget(event.target.id);
+                setActiveWidget(nextWidget === activeWidget ? null : nextWidget);
+              }}
+            ></CalciteAction>
+          </CalciteActionBar>
+
+          <CalcitePanel
+            heading="Layers"
+            height-scale="l"
+            width-scale="l"
+            data-panel-id="layers"
+            style={{ width: '18vw' }}
+            hidden
+          >
+            <CalciteList>
+              <CalciteListItem
+                label=""
+                description=""
+                value="land-acquisition"
+                ref={layerListDiv}
+              ></CalciteListItem>
+            </CalciteList>
+          </CalcitePanel>
+
+          <CalcitePanel
+            heading="Basemaps"
+            height-scale="l"
+            data-panel-id="basemaps"
+            style={{ width: '18vw' }}
+            hidden
+          >
+            <CalciteList>
+              <CalciteListItem
+                label=""
+                description=""
+                value="basemaps"
+                ref={calcitePanelBasemaps}
+              ></CalciteListItem>
+            </CalciteList>
+          </CalcitePanel>
+
+          <CalcitePanel heading="Description" data-panel-id="information" hidden>
+            {nextWidget === 'information' ? (
+              <div className="informationDiv">
+                <ul>
+                  <li>
+                    You can <b>filter utility data</b> by contract package, company, and data type:
+                    (point or line).
+                  </li>
+                  <br />
+                  <li>
+                    <b>Click the bar-chart series</b> to view progress on the respective company and
+                    work status over the map.
+                  </li>
+                  <br />
+                  <li>Click/unclick widgets icon for viewing Layer list, legend, and basemaps.</li>
+                </ul>
+              </div>
+            ) : (
+              <div className="informationDiv" hidden></div>
+            )}
+          </CalcitePanel>
+        </CalciteShellPanel>
+        <div
+          className="groundSwitchDiv"
+          style={{
+            position: 'fixed',
+            zIndex: 1,
+            bottom: 10,
+            right: '23%',
+            color: 'white',
+            backgroundColor: '#2b2b2b',
+            paddingLeft: 5,
+            paddingRight: 5,
+            paddingTop: 4,
+            paddingBottom: 4,
+          }}
+        >
+          Ground: {''}
+          On{' '}
+          <CalciteSwitch
+            onCalciteSwitchChange={(event: any) => setUnderground(event.target.checked)}
+          ></CalciteSwitch>{' '}
+          Off
+        </div>
+
+        <div className="mapDiv" ref={mapDiv}></div>
+      </CalciteShell>
+    </>
+  );
+}
+
+export default App;
